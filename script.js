9d3a814e3a49c4988ce4800d9c0f97104efbeb8d
siddheshkulkarni <siddhuk102@gmail.com>
2014-08-13 01:18:23 -0500

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+$(document).ready(function(){
+$("#composing").click(function(){
+$("#poem").slideToggle("slow");
+  });
+  
+});
+
+$(document).ready(function(){
+$("#education").click(function(){
+$("#msbe").slideToggle("slow");
+  });  
+});
+
+</script>
